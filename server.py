2413e6e9ea48b5ef4c7509f43456ab9822084f8c
bazpasha <bazpasha@gmail.com>
2016-07-10 13:15:31 +0300

    Updated

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 from flask import Flask
 from flask import render_template
 
+def get_data():
+    text = open('words/form_inf_sentence.txt', encoding='utf-8')
+
+    data = []
+    for line in text:
+        line = line.strip('\n')
+        line = line.split('\t')
+        s = [line[2], line[3], line[4]]
+        data.append(s)
+    return data
+
+tasks = get_data()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,9 +21,11 @@ def index():
 	return render_template('index.html');
 
 from flask import request, json
+import random
 @app.route('/task', methods=['GET'])
 def task():
-	return json.dumps({'task': 'необходимость', 'formal': 'necessity', 'informal': 'need'});
+	cur_task = random.choice(tasks)
+	return json.dumps({'task': cur_task[0], 'formal': cur_task[1], 'informal': cur_task[2]});
 	
 if __name__ == "__main__":
 	app.run()
