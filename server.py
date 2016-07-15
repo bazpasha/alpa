2dcf88507644be9bebdf181fa409d3843671e50b
bazpasha <bazpasha@gmail.com>
2016-07-15 20:25:46 +0300

    Code updated

--- server.py
+++ server.py
@@ -12,7 +12,18 @@ def get_data():
         data.append(s)
     return data
 
-tasks = get_data()
+formal_tasks = get_data()
+
+slang_tasks = [['arvo', 'afternoon', 'arrival'], ['attn', 'attention', 'attorney'], ['bc','because','be careful'], 
+               ['biz','business', 'busyness'], ['cmt', 'comment', 'computer'], ['cu', 'see you', 'cutie'], 
+               ['dju', 'did you', 'dejavu'], ['db', 'database', 'dumb'], ['exp', 'experience', 'exception'],
+               ['frag', 'kill', 'fragment'], ['fab', 'fabulous', 'fable'], ['hw', 'homework', 'hallway'],
+			   ['ic', 'I see', 'ice-creame'], ['ite', 'alright', 'iteration'], ['kk', 'ok', 'King Kong'],
+			   ['lil', 'little', 'lilac'], ['mnm', 'Eminem', 'minimum'], ['mty', 'empty', 'mighty'],
+			   ['ne', 'any', 'negative'], ['ni', 'no idea', 'night'], ['nme', 'enemy', 'not me'],
+			   ['ns', 'nice', 'anus'], ['nt', 'nice try', 'note'], ['nv', 'envy', 'never'], ['nw', 'no way', 'news'],
+			   ['pf', 'profile', 'pay for'], ['popo', 'police', 'postpone'], ['prd', 'period', 'product'],
+			   ['pz', 'peace', 'puzzle'], ['rm', 'room', 'remain']];
 
 app = Flask(__name__)
 
@@ -24,8 +35,12 @@ from flask import request, json
 import random
 @app.route('/task', methods=['GET'])
 def task():
-	cur_task = random.choice(tasks)
-	return json.dumps({'task': cur_task[0], 'formal': cur_task[1], 'informal': cur_task[2]});
+	mode = request.args.get('mode')
+	if mode == 'formal':
+		cur_task = random.choice(formal_tasks)
+	else:
+		cur_task = random.choice(slang_tasks)
+	return json.dumps({'task': cur_task[0], 'correct': cur_task[1], 'incorrect': cur_task[2]});
 	
 if __name__ == "__main__":
 	app.run()
